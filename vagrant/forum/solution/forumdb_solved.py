6d91b188b51b8aee7f711fc891e1e22793fc0d37
FlipeFrontDev <felipe.saint@gmail.com>
2022-02-10 10:15:18 -0300

    Add database strings

--- vagrant/forum/solution/forumdb_solved.py
+++ vagrant/forum/solution/forumdb_solved.py
@@ -1,23 +1,35 @@
-# Database code for the DB Forum, full solution!
+import psycopg2
 
-import psycopg2, bleach
+user = 'app'
+password = 'appaccess'
+host = '34.95.251.220'
+database = 'postgres'
 
-DBNAME = "forum"
 
 def get_posts():
-  """Return all posts from the 'database', most recent first."""
-  db = psycopg2.connect(database=DBNAME)
-  c = db.cursor()
-  c.execute("select content, time from posts order by time desc")
-  posts = c.fetchall()
-  db.close()
-  return posts
+    """Return all posts from the 'database', most recent first."""
+    db = psycopg2.connect(
+        user=user,
+        password=password,
+        host=host,
+        database=database)
+
+    c = db.cursor()
+    c.execute("select content, time from posts order by time desc")
+    db.close()
+    return c.fetchall()
+
 
 def add_post(content):
-  """Add a post to the 'database' with the current timestamp."""
-  db = psycopg2.connect(database=DBNAME)
-  c = db.cursor()
-  c.execute("insert into posts values (%s)", (bleach.clean(content),))  # good
-  db.commit()
-  db.close()
+    """Add a post to the 'database' with the current timestamp."""
 
+    db = psycopg2.connect(
+        user=user,
+        password=password,
+        host=host,
+        database=database
+    )
+
+    c = db.cursor()
+    c.execute("insert into posts values('%s')" % content)
+    db.commit()
